732abd91cb600458a7f6da9f9be61c10ec385edb
DOITEAU Matthieu <doiteau.matthieu@free.fr>
2013-08-12 14:51:54 +0200

    Project initialisation

---
+++ JAXBExample/src/main/java/the/md/project/JAXBExample/App.java
@@ -0,0 +1,13 @@
+package the.md.project.JAXBExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
